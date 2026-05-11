0fc7599bbed736e21db5a1df8b6c948dec066864
zero314 <zero3.14159265358979323846@gmail.com>
2026-05-11 08:14:06 +0800

    新增基本多媒體播放器（支援音樂與影片）
    新增 WPF MediaElement 至 WinForms，實作開啟、播放、停止多媒體檔案功能。調整介面，加入操作按鈕與狀態顯示，支援 mp3、wav、mp4、avi、wmv 等格式。專案檔加入 WPF 相關參考。

--- 1121538_徐霈綺_多媒體撥放器/Form1.Designer.cs
+++ 1121538_徐霈綺_多媒體撥放器/Form1.Designer.cs
@@ -29,12 +29,92 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.videoPanel = new System.Windows.Forms.Integration.ElementHost();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "開啟檔案";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(93, 12);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "播放";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            this.btnPlay.Enabled = false;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(174, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            this.btnStop.Enabled = false;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(255, 17);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(65, 12);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "尚未選擇檔案";
+            // 
+            // videoPanel
+            // 
+            this.videoPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.videoPanel.BackColor = System.Drawing.Color.Black;
+            this.videoPanel.Location = new System.Drawing.Point(12, 41);
+            this.videoPanel.Name = "videoPanel";
+            this.videoPanel.Size = new System.Drawing.Size(776, 397);
+            this.videoPanel.TabIndex = 4;
+            this.videoPanel.Text = "elementHost1";
+            this.videoPanel.Child = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.videoPanel);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "多媒體播放器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Integration.ElementHost videoPanel;
     }
 }
 
--- 1121538_徐霈綺_多媒體撥放器/Form1.cs
+++ 1121538_徐霈綺_多媒體撥放器/Form1.cs
@@ -7,14 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Controls;
 
 namespace _1121538_徐霈綺_多媒體撥放器
 {
     public partial class Form1 : Form
     {
+        private string currentFilePath = "";
+        private MediaElement wpfMediaElement;
+
         public Form1()
         {
             InitializeComponent();
+            InitializeMediaPlayer();
+        }
+
+        private void InitializeMediaPlayer()
+        {
+            wpfMediaElement = new MediaElement();
+            wpfMediaElement.LoadedBehavior = MediaState.Manual;
+            wpfMediaElement.UnloadedBehavior = MediaState.Stop;
+            videoPanel.Child = wpfMediaElement;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "多媒體檔案|*.mp3;*.wav;*.mp4;*.avi;*.wmv|音樂檔案|*.mp3;*.wav|影片檔案|*.mp4;*.avi;*.wmv|所有檔案|*.*";
+                ofd.Title = "選擇要播放的多媒體檔案";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    StopMedia();
+                    currentFilePath = ofd.FileName;
+                    wpfMediaElement.Source = new System.Uri(currentFilePath);
+                    lblStatus.Text = "已選擇: " + System.IO.Path.GetFileName(currentFilePath);
+                    btnPlay.Enabled = true;
+                    btnStop.Enabled = true;
+                }
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFilePath)) return;
+
+            wpfMediaElement.Play();
+
+            lblStatus.Text = "正在播放: " + System.IO.Path.GetFileName(currentFilePath);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopMedia();
+            if (!string.IsNullOrEmpty(currentFilePath))
+            {
+                lblStatus.Text = "已停止: " + System.IO.Path.GetFileName(currentFilePath);
+            }
+        }
+
+        private void StopMedia()
+        {
+            if (wpfMediaElement != null)
+            {
+                wpfMediaElement.Stop();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopMedia();
         }
     }
 }
